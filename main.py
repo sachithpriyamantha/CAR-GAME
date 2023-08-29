14d391f15a35a17f0e6be29aa57b63d630b8a883
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2023-08-29 21:03:00 +0530

    update

--- main.py
+++ main.py
@@ -15,16 +15,27 @@ pygame.display.set_caption('Car Game')
 
 #colors
 grey = (100, 100, 100)
-green = (76, 208, 56)
+darkgreen = (0,100,0)
 red = (200, 0, 0)
 white = (255, 255, 255)
 yellow = (255, 232, 0)
+darkgray = (169,169,169)
+dodgerblue4 = (16,78,139)
 
 
 #game setting
 gameover = False
 speed = 2
 score = 0
+
+#markers size
+marker_width = 10
+marker_height = 50
+
+#road and edge markers
+road = (100, 0, 300, height)
+left_edge_marker = (95, 0, marker_width, height)
+right_edge_marker = (395, 0, marker_width, height)
 
 #game loop
 clock = pygame.time.Clock()
@@ -39,7 +50,14 @@ while running:
     
 
     #drow the grass
-    screan.fill(green)
+    screan.fill(darkgreen)
+
+    #draw the road
+    pygame.draw.rect(screan, darkgray, road)
+
+    #draw the edge marker
+    pygame.draw.rect(screan, dodgerblue4, left_edge_marker)
+    pygame.draw.rect(screan, dodgerblue4, right_edge_marker)
 
     pygame.display.update()
 
