1c1692274ec00579e475905bb049ee5a88bf1a89
sachith-priyamantha <89736781+sachithpriyamantha@users.noreply.github.com>
2023-08-29 20:24:02 +0530

    update

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ height = 500
 screan_size = (width, height)
 screan = pygame.display.set_mode(screan_size)
 pygame.display.set_caption('Car Game')
+
+#colors
+grey = (100, 100, 100)
+green = (76, 208, 56)
+red = (200, 0, 0)
+white = (255, 255, 255)
+yellow = (255, 232, 0)
